8ecdeb88544bb2bcc2395222844ec684d8e6c43f
PC <pc@TheLaptop>
2026-05-12 13:48:33 -0700

    make LeaderBoard display data in list

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,8 +1,22 @@
 import {useState} from 'react';
+const example= {
+  userName: "example_Player",
+  Score: 2000,
+}
+
+const leaderBoardList = [example,example,example,example,example];
+
+
 
 function Leaderboard({setScreen}) {
   return (
-    <div>Should add leaderboard to this file later</div>
+    <div> 
+          <p>LearderBoard</p>
+      <ul>
+        <p>test</p>
+        <p>test</p>
+        {leaderBoardList.map((player,index) => (<li key = {index}>{player.userName + '  ' + player.Score}</li>))}</ul>
+    </div>
   );
 }
 export default Leaderboard;
