dfaa77a5ed42d22a4043900974951db54aac5824
PC <pc@TheLaptop>
2026-05-12 14:30:31 -0700

    chang style of Leaderboard.jsx

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+
 const example= {
   userName: "example_Player",
   Score: 2000,
@@ -11,11 +12,17 @@ const leaderBoardList = [example,example,example,example,example];
 function Leaderboard({setScreen}) {
   return (
     <div> 
-          <p>LearderBoard</p>
-      <ul>
-        <p>test</p>
-        <p>test</p>
-        {leaderBoardList.map((player,index) => (<li key = {index}>{player.userName + '  ' + player.Score}</li>))}</ul>
+          <p style={{fontSize:30}}>LeaderBoard</p>
+      <ul style={{listStyleType:'none'}}>
+        <div style={{columnCount:2,fontSize:25}}><li>Player</li> <li>Score</li></div>
+        {leaderBoardList.map((player,index) => (<li key = {index}>
+          <ul style={{listStyleType:'none', }}>
+            <div style={{columnCount:2}}>
+              <li>{player.userName}</li> <li>{player.Score}</li>
+            </div>
+          </ul>
+          </li>))}
+      </ul>
     </div>
   );
 }
