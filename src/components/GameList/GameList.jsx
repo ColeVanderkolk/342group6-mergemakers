06ac4e0c4f063c9898002ed9bcc294f316cf5398
lily <lily@DESKTOP-GBL5REE.localdomain>
2026-05-26 15:52:49 -0700

    added Games title and white line

--- src/components/GameList/GameList.jsx
+++ src/components/GameList/GameList.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import asteroidImage from '../GamePlay/sprites/Ship_active.png';
 import brickImage from '../GamePlay/sprites/BrickBreaker.png';
 import pongImage from '../GamePlay/sprites/pong_btn_image.png';
+import lineImage from '../white_line.png';
 
 import './GameList.css'
 
@@ -28,15 +29,18 @@ function GameList() {
     ];
 
     return (
-        <nav className="game-row">
+        <div>
+            <p className='title'>Games</p>
+            <img className="white-line" src={lineImage} alt = ""/>
+            <nav className="game-row">
+                {games.map((game) => (
+                    <Link key={game.title} to={game.gameLink} className="game-link">
+                        <Game game={game} />
+                    </Link>
+                ))}
+            </nav>          
+        </div>
 
-            {games.map((game) => (
-                <Link key={game.title} to={game.gameLink} className="game-link">
-                    <Game game={game} />
-                </Link>
-            ))}
-
-        </nav>
     );
 }
 
